346cf26d3017e236e48c49973f5f69a71f33338c
Hiago Granelli <hiagogranelli12@gmail.com>
2024-06-24 21:54:24 -0300

    add nvim config

---
+++ lua/josean/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme rose-pine]])
+    end,
+  },
+}
