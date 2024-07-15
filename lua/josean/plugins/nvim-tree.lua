a90504a6d6b947705a0bf81d9488f065d1318f9a
Hiago Granelli <hiagogranelli12@gmail.com>
2024-07-15 16:59:01 -0300

    change nvim tree side to right

--- lua/josean/plugins/nvim-tree.lua
+++ lua/josean/plugins/nvim-tree.lua
@@ -12,6 +12,7 @@ return {
       view = {
         width = 35,
         relativenumber = true,
+        side = "right",
       },
       -- change folder arrow icons
       renderer = {
@@ -52,5 +53,5 @@ return {
     keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
-  end
+  end,
 }
